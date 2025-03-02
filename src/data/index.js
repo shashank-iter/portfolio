6101a397c3005dbe86dd393828c159f033fed02f
shashank-iter <pandeyshashank812@gmail.com>
2025-03-03 02:46:03 +0530

    added run and legend Data for wagon wheel project

---
+++ src/data/index.js
@@ -0,0 +1,46 @@
+export const runsData1 = [
+  { runs: 2, angle: 14, length: 140 },
+  { runs: 4, angle: 67, length: 180 },
+  { runs: 3, angle: 119, length: 160 },
+  { runs: 6, angle: 23, length: 190 },
+  { runs: 1, angle: 191, length: 100 },
+  { runs: 2, angle: 278, length: 120 },
+  { runs: 4, angle: 51, length: 180 },
+  { runs: 3, angle: 222, length: 150 },
+  { runs: 6, angle: 340, length: 190 },
+  { runs: 1, angle: 98, length: 110 },
+]
+
+export const runsData2 = [
+  { runs: 3, angle: 42, length: 170 },
+  { runs: 2, angle: 201, length: 140 },
+  { runs: 6, angle: 13, length: 190 },
+  { runs: 4, angle: 75, length: 180 },
+  { runs: 1, angle: 283, length: 90 },
+  { runs: 2, angle: 123, length: 160 },
+  { runs: 3, angle: 311, length: 150 },
+  { runs: 6, angle: 56, length: 190 },
+  { runs: 4, angle: 208, length: 180 },
+  { runs: 1, angle: 147, length: 120 },
+]
+
+export const runsData3 = [
+  { runs: 2, angle: 29, length: 130 },
+  { runs: 6, angle: 94, length: 190 },
+  { runs: 3, angle: 167, length: 160 },
+  { runs: 4, angle: 41, length: 180 },
+  { runs: 1, angle: 256, length: 110 },
+  { runs: 2, angle: 135, length: 140 },
+  { runs: 6, angle: 19, length: 190 },
+  { runs: 3, angle: 289, length: 150 },
+  { runs: 4, angle: 63, length: 180 },
+  { runs: 1, angle: 216, length: 100 },
+]
+
+export const legendData = [
+    { runs: 1, color: "bg-white", label: "1 Run" },
+    { runs: 2, color: "bg-yellow-400", label: "2 Runs" },
+    { runs: 3, color: "bg-indigo-500", label: "3 Runs" },
+    { runs: 4, color: "bg-blue-500", label: "4 Runs" },
+    { runs: 6, color: "bg-red-500", label: "6 Runs" },
+  ];
